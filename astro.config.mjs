ef00383366f6a11361cd60751dd74ff82a034991
FreakyBanana-yellow <felix-behringer@gmx.de>
2025-06-30 11:39:47 +0200

    fix: netlify build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
-  integrations: [tailwind()],
   vite: {
-  plugins: [tailwind()],
-},
-});
+    plugins: [tailwind()],
+  },
+})
+

19470e76a2bfc70e2399b9c74907abb78b42ce35
FreakyBanana-yellow <felix-behringer@gmx.de>
2025-07-11 20:21:22 +0200

    Update: BaseLayout mit VIP Bot Login, Featuresteuerung

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [react()],
-
-export default defineConfig({
-  integrations: [tailwind()],
-})
+  integrations: [react(), tailwind()],
+  vite: {
+    ssr: {
+      external: ["react", "react-dom"]
+    }
+  }
+});
